07f28bddced76f859b40a2f2d5b08c129ee99df6
64andy <64andy2000@gmail.com>
2024-12-02 20:14:10 +1300

    Added some docs to ISolution

--- 2024/AdventOfCode2024/src/ISolution.cs
+++ 2024/AdventOfCode2024/src/ISolution.cs
@@ -1,7 +1,11 @@
 namespace AdventOfCode2024.src;
 
+/// <summary>
+/// Interface that each day's solver class should implement
+/// </summary>
 interface ISolution
 {
+    /// <returns>The day of the current solution as a number</returns>
     int GetDay();
 
     string Part1(string[] input);
