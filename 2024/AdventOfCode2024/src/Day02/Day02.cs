a5893d99027f5b2fba09db0e2a35a74128420110
64andy <64andy2000@gmail.com>
2024-12-03 11:15:52 +1300

    2024 Day 2 Part 2 solved

--- 2024/AdventOfCode2024/src/Day02/Day02.cs
+++ 2024/AdventOfCode2024/src/Day02/Day02.cs
@@ -1,11 +1,16 @@
 namespace AdventOfCode2024.src.Day02;
 
+/// <summary>
+/// We are given a list of reports a.k.a. levels, where each line
+/// is a space separated list of numbers
+/// </summary>
 class Day02 : ISolution
 {
     public int GetDay() => 2;
 
     private static bool IsSafe(int[] level)
     {
+        // Note: This would've been easier if I'd converted to deltas
         const int MIN_DIFF = 1;
         const int MAX_DIFF = 3;
 
@@ -27,6 +32,11 @@ class Day02 : ISolution
         return true;
     }
 
+    /// <summary>
+    /// A report is considered "safe" if all numbers are increasing/decreasing,
+    /// AND the difference between each adjacent number is between [1,3] inclusive
+    /// </summary>
+    /// <returns>The number of "safe" reports</returns>
     public string Part1(string[] input)
     {
         // Step 1: Parse to ints
@@ -43,8 +53,32 @@ class Day02 : ISolution
         return numSafe.ToString();
     }
 
+    /// <summary>
+    /// Returns all versions of the level where one item's missing
+    /// </summary>
+    private static IEnumerable<int[]> AllListsMinusOneElement(int[] level) {
+        for (int i=0; i<level.Length; i++) {
+            yield return level.Where((_, idx) => idx != i).ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Now we check if the list can be safe, if we remove at most one element
+    /// </summary>
+    /// <returns>The number of possibly "safe" reports</returns>
     public string Part2(string[] input)
     {
-        return "INCOMPLETE";
+        // Step 1: Parse to ints
+        int[][] levels = input.Select(line => line.Split(' '))
+                        .Select(nums => nums.Select(int.Parse).ToArray())
+                        .ToArray();
+
+        int numSafe = 0;
+        foreach (var level in levels)
+        {
+            if (AllListsMinusOneElement(level).Any(IsSafe)) numSafe++;
+        }
+
+        return numSafe.ToString();
     }
 }
