9a3c14fa93050fcde2380334a10a43734ed46e4b
64andy <64andy2000@gmail.com>
2024-12-04 11:37:26 +1300

    2024 Day 3 Part 2 solved (praise regex)

--- 2024/AdventOfCode2024/src/Day03/Day03.cs
+++ 2024/AdventOfCode2024/src/Day03/Day03.cs
@@ -22,15 +22,49 @@ class Day03 : ISolution
         Regex pattern = new(@"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
 
         return input.SelectMany(line => pattern.Matches(line))      // Flatten from lines of matches to simply all matches
-                .Select(m => m.Groups.Values.Skip(1).Take(2))       // The first entry in regex matches is the whole string, ignore it
+                .Select(m => m.Groups.Values.Skip(1).Take(2))       // The first entry in regex matches is the matched substr, ignore it
                 .Select(e => int.Parse(e.First().Value) * int.Parse(e.Last().Value))// Multiply both sides
                 .Sum()
                 .ToString();
-
     }
 
+    /// <summary>
+    /// Similar logic, however we now look for "do()" and "don't()" strings.
+    /// - "do()" means multiply any proceeding mul's
+    /// - "don't()" means ignore any proceeding mul's
+    /// We start in a "do()" state, and use/ignore until it changes
+    /// </summary>
+    /// <returns>The sum of all non-ignored mul's</returns>
     public string Part2(string[] input)
     {
-        return "UNIMPLEMENTED";
+        // I'm so glad I know regex
+        Regex pattern = new(@"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)", RegexOptions.Compiled);
+
+        bool enabled = true;
+        int sum = 0;
+        // Match, flatten, and grab the groups
+        var entries = input.SelectMany(line => pattern.Matches(line))
+                            .Select(t => t.Groups.Values);
+        foreach (var match in entries)
+        {
+            // Reminder: The first "group" is always the matched substr
+            if (match.First().Value == "do()")
+            {
+                enabled = true;
+            }
+            else if (match.First().Value == "don't()")
+            {
+                enabled = false;
+            }
+            else if (enabled)
+            {
+                sum += match.Skip(1)
+                            .Select(i => int.Parse(i.Value))
+                            .Aggregate(1, (a, b) => a * b);
+
+            }
+        }
+
+        return sum.ToString();
     }
 }
