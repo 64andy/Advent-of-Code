f915e83a73ca15a8c2478479b8e05f000493c1c6
64andy <64andy2000@gmail.com>
2024-12-05 10:51:43 +1300

    Day04 2024 Part 2 complete

--- 2024/src/4/Day04.cs
+++ 2024/src/4/Day04.cs
@@ -1,8 +1,22 @@
 namespace AdventOfCode2024.src.Day04;
+
+/// <summary>
+/// Our input is a grid of characters, all of them either 'X', 'M', 'A', or 'S'
+/// </summary>
 class Day04 : ISolution
 {
     public int GetDay() => 4;
 
+    /// <summary>
+    /// Check if we can find the <c>needle</c> in the <c>grid</c> by moving in a given <c>direction</c> from <c>startPos</c>
+    /// </summary>
+    /// <param name="grid">The grid of all letters</param>
+    /// <param name="needle">The string we're looking for</param>
+    /// <param name="startPos">The <c>(i,j)</c> coordinates we start from</param>
+    /// <param name="direction">The <c>(i,j)</c> values we add to our coordinates on each step.
+    ///                         e.g. (0,1) means we search by moving right, (1,-1) means we look bottom-left...
+    /// </param>
+    /// <returns><c>true</c> if the needle is in the given position with the given direction</returns>
     private static bool FindWordInDirection(string[] grid, string needle, (int, int) startPos, (int, int) direction)
     {
         var (i, j) = startPos;
@@ -25,6 +39,11 @@ class Day04 : ISolution
         return true;
     }
 
+    /// <summary>
+    /// In the given list, "XMAS" occurs multiple times.
+    /// They can occur horizontally, vertically, and diagonally, both forwards and backwards.
+    /// </summary>
+    /// <returns>The number of occurances of "XMAS"</returns>
     public string Part1(string[] input)
     {
         (int, int)[] directions = [
@@ -46,8 +65,43 @@ class Day04 : ISolution
         return total.ToString();
     }
 
+    /// <summary>
+    /// Checks if "MAS" occurs diagonally in both directions at the given position.
+    /// e.g.
+    /// M.S
+    /// .A.
+    /// M.S
+    /// 
+    /// Warning: There is no bounds checking, this isn't a problem if you
+    /// simply don't give coords on the edges of the grid.
+    /// </summary>
+    private static bool IsMasInACross(string[] grid, int i, int j)
+        // Letter in the middle must always be an 'A'
+        => grid[i][j] == 'A'
+        && (
+            // Top-Left & Bottom-Right diagonal check
+            grid[i - 1][j - 1] == 'M' && grid[i + 1][j + 1] == 'S'
+            || grid[i - 1][j - 1] == 'S' && grid[i + 1][j + 1] == 'M'
+        ) && (
+            // Bottom-Left & Top-Right diagonal check
+            grid[i + 1][j - 1] == 'M' && grid[i - 1][j + 1] == 'S'
+            || grid[i + 1][j - 1] == 'S' && grid[i - 1][j + 1] == 'M'
+        );
+
+    /// <summary>
+    /// Turns out "XMAS" is "MAS in an X shape"; where "MAS" occurs diagonally both ways
+    /// </summary>
+    /// <returns>The number of occurances of "MAS" in an X shape</returns>
     public string Part2(string[] input)
     {
-        return "UNIMPLEMENTED";
+        // Note: We're enumerating indices, however we're staying away from the edges,
+        // since they can't be a cross as one of their sides is out of bounds.
+        return Enumerable.Range(1, input.Length - 2)
+                    // An iterator of all (i,j indices)
+                    .SelectMany(i => Enumerable.Range(1, input[i].Length - 2).Select(j => (i, j)))
+                    // Is there a cross in this position
+                    .Where(t => IsMasInACross(input, t.i, t.j))
+                    .Count()
+                    .ToString();
     }
 }
