bb18d623f770d58b8c28d8a2d9e0dc51ef7b2d72
64andy <64andy2000@gmail.com>
2024-12-30 17:15:46 +1300

    Cleaned up the day 11 code and added comments

--- 2024/src/11/Day11.cs
+++ 2024/src/11/Day11.cs
@@ -7,65 +7,87 @@ class Day11 : ISolution
 {
     public int GetDay() => 11;
 
-    private static IEnumerable<KeyValuePair<string, long>> ApplyRules(KeyValuePair<string, long> pair)
+    private static IEnumerable<string> ApplyRules(string key)
     {
-        var (key, count) = pair;
         if (key == "0")
         {
-            return [new("1", count)];
+            return ["1"];
         }
         else if (key.Length % 2 == 0)
         {
             long firstHalf = long.Parse(key[..(key.Length / 2)]);
-            long secondHalf = long.Parse(key.Substring(key.Length/2, key.Length/2));
-            return [new(firstHalf.ToString(), count), new(secondHalf.ToString(), count)];
+            long secondHalf = long.Parse(key.Substring(key.Length / 2, key.Length / 2));
+            return [firstHalf.ToString(), secondHalf.ToString()];
         }
         else
         {
-            return [new((long.Parse(key)*2024).ToString(), count)];
+            return [(long.Parse(key) * 2024).ToString()];
         }
     }
 
-    private static Dictionary<string, long> CrushIntoDict(IEnumerable<KeyValuePair<string, long>> pairs)
+    /// <summary>
+    /// Turns a stream of KeyValuePairs into a dictionary, adding together
+    /// any pair's values that share a key.
+    /// </summary>
+    private static Dictionary<string, long> SumIntoDict(IEnumerable<KeyValuePair<string, long>> pairs)
     {
         Dictionary<string, long> output = [];
-        foreach (var kv in pairs)
+        foreach (var (key, val) in pairs)
         {
-            if (output.TryGetValue(kv.Key, out var count))
-            {
-                output[kv.Key] = count + kv.Value;
-            }
+            if (output.TryGetValue(key, out var count))
+                output[key] = count + val;
             else
-            {
-                output[kv.Key] = kv.Value;
-            }
+                output[key] = val;
         }
 
         return output;
     }
 
+    /// <summary>
+    /// Calculates the number of blinks
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <param name="numBlinks">How many times the logic should be run</param>
+    /// <returns></returns>
     private static long CalculateNumStones(string input, int numBlinks)
     {
-        Dictionary<string, long> occurrences = [];
-        foreach (string val in input.Split())
-        {
-            long count = occurrences.GetValueOrDefault(val, 0);
-            occurrences[val] = count+1;
-        }
+        // Count the number of initial stones
+        Dictionary<string, long> occurrences = SumIntoDict(
+            input.Split().Select(s => new KeyValuePair<string, long>(s, 1))
+        );
 
-        for (int i=0; i<numBlinks; i++)
+        for (int i = 0; i < numBlinks; i++)
         {
-            occurrences = CrushIntoDict(occurrences.SelectMany(ApplyRules));
+            var transformed = occurrences.SelectMany(
+                // Split the strings
+                kv => ApplyRules(kv.Key)
+                    // Then reapply the "count" from earlier
+                    .Select(key => new KeyValuePair<string, long>(key, kv.Value))
+            );
+            occurrences = SumIntoDict(transformed);
         }
 
         return occurrences.Select(kv => kv.Value).Sum();
     }
 
+    /// <summary>
+    /// We have a line of "rocks", which each contain a number.
+    /// Every blink these numbers change:
+    /// - If the number is 0, it turns into a 1
+    /// - Else if the number is even, it splits in half ('1201' -> ['12', '01'] -> [12, 1])
+    /// - Else the number is multiplied by 2024 ('100' => '202400')
+    /// 
+    /// Be warned, the number of rocks grows VERY fast
+    /// </summary>
+    /// <returns>The number of stones remaining after 25 "blinks"</returns>
     public string Part1(string[] input)
     {
         return CalculateNumStones(input[0], 25).ToString();
     }
 
+    /// <summary>
+    /// We've increased the required number of blinks to 75
+    /// </summary>
     public string Part2(string[] input)
     {
         return CalculateNumStones(input[0], 75).ToString();
